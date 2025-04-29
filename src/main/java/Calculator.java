0131014e3684d27fc1881011f57862d68a400482
jisoo78 <ekelldks@naver.com>
2025-04-29 16:36:44 +0900

    feat: Calculator mission

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    public static int add(int num1, int num2) {
+        // int 정수형으로 반환
+        // add, subtract, multiply, divide 메소드 사용
+        // return 을 통한 값 반환
+        return num1 + num2;
+    }
+
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        해당 메소드 불러오기
+        System.out.println("두 수의 합: " + Calculator.add(5,4));
+        System.out.println("두 수의 차: " + Calculator.subtract(10,3));
+        System.out.println("두 수의 곱: " + Calculator.multiply(5,9));
+        System.out.println("두 수의 몫: " + Calculator.divide(100,7));
+
+    }
+}
