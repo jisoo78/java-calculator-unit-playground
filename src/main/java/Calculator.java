a478f653bbe6b9b7e30292f3806e77bcd17977a8
jisoo78 <ekelldks@naver.com>
2025-04-30 21:06:54 +0900

    3단계 - 문자열 계산기 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,24 +1,75 @@
 public class Calculator {
 
+    /**
+     * 일단 1,2 -> 3 ,:은 합으로 나온다
+     */
 
-    public static int add(int num1, int num2) {
-        // int 정수형으로 반환
-        // add, subtract, multiply, divide 메소드 사용
-        // return 을 통한 값 반환
-        return num1 + num2;
+
+    // 입력값이 2,3,4  => 9가 나와야한다
+    // 반복문을 돌려서 입력값에서 ,: 가 있다면 +로 대체 기본값은 +
+    // 차라리 switch case 를 통해 사칙연산?
+    public static int strAdd(String expression) {
+        String[] tokens = expression.split("[,|:]");
+
+        // 일단 각 ", :"을 정규표현식을 활용해 구분
+        // 숫자의 합을 구현
+        int total = 0;
+
+        for (String token : tokens) {
+            total += addPlus(token);
+            // expression2 로 값 전달??
+        }
+        // 향상된 for 문 사용
+
+        return total;
     }
 
-    public static int subtract(int num1, int num2) {
-        return num1 - num2;
+    private static int addPlus(String expression2) {
+        int num = 0;
+        int total = 0;
+        char sign = '+';
+        // 초기값 +로 설정
+
+
+        for (int i = 0; i <= expression2.length(); i++) {
+            char ch;
+            // 반복문 내 사용하는 변수 선언
+            // 기호를 저장하여 isDigit 함수로 참 거짓 판별
+            if (i < expression2.length()) {
+                ch = expression2.charAt(i);
+            } else {
+                ch = '+';
+            }
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+                // 여러 자리 숫자 처리를 위한 공식
+            } else {
+                switch (sign) {
+                    case '+':
+                        total += num;
+                        break;
+                    case '-':
+                        total -= num;
+                        break;
+                    case '*':
+                        total *= num;
+                        break;
+                    case '/':
+                        total /= num;
+                        break;
+                }
+                num = 0;
+                sign = ch;
+            }
+
+        }
+        return total;
     }
-
-    public static int multiply(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    public static int divide(int num1, int num2) {
-        return num1 / num2;
-    }
-
-
 }
+
+
+
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,32 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        해당 메소드 불러오기
-//        System.out.println("두 수의 합: " + Calculator.add(5,4));
-//        System.out.println("두 수의 차: " + Calculator.subtract(10,3));
-//        System.out.println("두 수의 곱: " + Calculator.multiply(5,9));
-//        System.out.println("두 수의 몫: " + Calculator.divide(100,7));
-        //System.out.println("메소드 출력: " + Calculator.qwe(43));
-//        Calculator.qwe(43);
-
 
         Scanner scanner = new Scanner(System.in);
         // 사용자에게 숫자를 입력받아 사칙연산 돌려보기
 
-        System.out.println("첫 번째 숫자 입력");
-        int num1 = scanner.nextInt();
-        System.out.println("두 번쨰 숫자 입력");
-        int num2 = scanner.nextInt();
 
-        int total1 = Calculator.add(num1, num2);
-        int total2 = Calculator.subtract(num1, num2);
-        int total3 = Calculator.multiply(num1, num2);
-        int total4 = Calculator.divide(num1, num2);
+        System.out.println("문자열을 입력해주세요");
+        String expression = scanner.nextLine();
 
-        System.out.println("두 수의 합: " + total1);
-        System.out.println("두 수의 차: " + total2);
-        System.out.println("두 수의 곱: " + total3);
-        System.out.println("두 수의 몫: " + total4);
+        String total5 = String.valueOf(Calculator.strAdd(expression));
+
+        System.out.println("문자열의 합: " + total5);
 
     }
 }
