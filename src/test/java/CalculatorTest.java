a872900205ad6554f756b274cea1e090a806bbed
jisoo78 <ekelldks@naver.com>
2026-02-16 18:52:13 +0900

    test: 나눗셈 테스트

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -51,4 +51,19 @@ public class CalculatorTest {
         int actual = 12;
         assertEquals(actual, expected);
     }
+
+    @Test
+    @DisplayName("사칙연산 중 나눗셈의 기능을 구현할 수 있다")
+    public void  test_division() {
+        // given
+        int a = 10;
+        int b = 2;
+
+        Calculator calculator = new Calculator();
+        // when
+        int expected = calculator.division(a, b);
+        // then
+        int actual = 5;
+        assertEquals(actual, expected);
+    }
 }
